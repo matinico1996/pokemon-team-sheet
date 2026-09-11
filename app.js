4e467b9962d07309087d089e7316cfe852f01896
matinico1996 <matiasahumada568@gmail.com>
2026-09-11 16:06:56 -0300

    Update Friday lockdown schedule to start at 18:00

--- app.js
+++ app.js
@@ -1115,8 +1115,8 @@ async function main() {
 
     // Jueves (4): 14:30 a 18:00
     if (day === 4 && time >= 14.5 && time < 18.0) return true;
-    // Viernes (5): 18:20 a 21:00
-    if (day === 5 && time >= (18 + 20/60) && time < 21.0) return true;
+    // Viernes (5): 18:00 a 21:00
+    if (day === 5 && time >= 18.0 && time < 21.0) return true;
     // Sábado (6): 11:30 a 15:00
     if (day === 6 && time >= 11.5 && time < 15.0) return true;
 
